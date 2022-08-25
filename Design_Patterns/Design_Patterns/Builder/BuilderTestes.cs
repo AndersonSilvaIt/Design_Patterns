1869df28143306e29ca5a76839621603d9f2d489
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-08-25 08:03:18 -0300

    Implementado o deign builder

---
+++ Design_Patterns/Design_Patterns/Builder/BuilderTestes.cs
@@ -0,0 +1,27 @@
+﻿namespace Design_Patterns.Builder
+{
+    public static class BuilderTestes
+    {
+        public static void ExecuteTests()
+        {
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.BuildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director
+            // class.
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+        }
+    }
+}
---
+++ Design_Patterns/Design_Patterns/Builder/Interfaces/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace Design_Patterns.Builder.Interfaces
+{
+    public interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+}
